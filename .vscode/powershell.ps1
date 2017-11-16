77c1ffe83e9182ede786b72bdce71f217e609a53
김경환 <raisehim@kokomogames.co.kr>
2017-11-16 19:08:49 +0900

    up

---
+++ .vscode/powershell.ps1
@@ -0,0 +1,16 @@
+﻿if( $args.length -eq 0 ){ # 바로 터미널 진입시 git integrated shell을 연다
+    powershell -noexit -ExecutionPolicy Unrestricted "& './.vscode/kokomo.ps1'"
+}
+else { # Task 등을 실행하는 경우
+
+    $tmp, $argsOthers = $args
+
+    if( $argsOthers -eq "node_modules\.bin\grunt.cmd" ) { # Grunt 인자가 쪼개져서 들어오는 현상 때문에 1개로 합침
+        $gruntCmd, $gruntArgs = $argsOthers
+        Invoke-Expression "$($gruntCmd) '$($gruntArgs -join ' ')'"
+    }
+    else {
+        Invoke-Expression "$argsOthers -join ' '"
+    }
+
+}
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict;'
 
 const Node = require('./node');
+const Sensor = require('./sensor');
 
 const startBT = async () => {
-
 };
 
 const testBT = async () => {
@@ -11,5 +11,8 @@ const testBT = async () => {
 };
 
 
-module.exports = testBT()
+let BT = module.exports = testBT()
     .then(console.info, console.error);
+
+BT.Node = Node;
+BT.Sensor = Sensor;
